9cfc2a336971ae511401921716732c65feb32b9d
Sebastian Pineda <Sebastian.Pineda@bloomy.com>
2023-08-10 08:47:57 -0400

    Added new exception catching

--- CCSVInterface.cpp
+++ CCSVInterface.cpp
@@ -1,6 +1,7 @@
 #include "CCSVInterface.h"
 
 #include <stdint.h>
+#include <iostream>
 
 #include "Dataframe.h"
 
@@ -22,5 +23,11 @@ void print_Dataframe_head_float(Dataframe frame, uint64_t n_rows) {
 }
 
 float col_name_mean_float(Dataframe frame, const char* col_name) {
-  return reinterpret_cast<read_file::Dataframe<float>*>(frame)->Mean(col_name);
+  try {
+    return reinterpret_cast<read_file::Dataframe<float>*>(frame)->Mean(
+        col_name);
+  } catch (ColumnNotFoundException& e) {
+    std::cout << "Column not found" << std::endl;
+    return 0.0f;
+  }
 }
