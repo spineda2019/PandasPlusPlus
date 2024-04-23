124851679b95cf9bcde67779582247db4f335d0a
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-23 14:59:44 -0400

    test complex numbers

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include <complex>
 #include <iostream>
 #include <ostream>
 #include <ppp/Matrix.hpp>
@@ -20,9 +21,21 @@ void TestEmptyPrint() {
     ppp::Matrix<float> empty{};
 
     std::cout << empty << std::endl;
-};
+}
+
+void TestComplexPrint() {
+    std::vector<std::vector<std::complex<float>>> complex{{
+        {std::complex<float>(3.0, 3.0), std::complex<float>(4.0, 4.0)},
+        {std::complex<float>(5.0, 5.0), std::complex<float>(6.0, 6.0)},
+    }};
+
+    ppp::Matrix<std::complex<float>> cmp{complex};
+
+    std::cout << cmp << std::endl;
+}
 
 int main(void) {
     TestHeadlessPrint();
     TestEmptyPrint();
+    TestComplexPrint();
 }
