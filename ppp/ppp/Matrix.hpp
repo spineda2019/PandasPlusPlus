4921fab246232520b8ebdbab88b21dffe040f8a1
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-23 11:35:26 -0400

    apply LGPL3.0 to source files

--- ppp/ppp/Matrix.hpp
+++ ppp/ppp/Matrix.hpp
@@ -1,3 +1,23 @@
+/*
+ *  Matrix.hpp
+ *  Datatype class for use with common linear algebra operations
+ *
+ *  Copyright (C) 2024 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License Version 3.0 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License and GNU Lesser General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef PPP_PPP_MATRIX_HPP
 #define PPP_PPP_MATRIX_HPP
 
