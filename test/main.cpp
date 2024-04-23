1eb311f9580034eda06f6a908342dfc9d4b00418
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-23 14:33:36 -0400

    formalize tests into structure

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,28 @@
+#include <iostream>
+#include <ostream>
 #include <ppp/Matrix.hpp>
 #include <vector>
 
+void TestHeadlessPrint() {
+    std::vector<std::vector<float>> data{
+        {3.0, 4.0},
+        {3.0, 4.0},
+    };
+
+    ppp::Matrix lvalue{data};
+
+    data = {};
+
+    std::cout << lvalue << std::endl;
+}
+
+void TestEmptyPrint() {
+    ppp::Matrix<float> empty{};
+
+    std::cout << empty << std::endl;
+};
+
 int main(void) {
-    ppp::Matrix simple{std::vector{
-        std::vector<float>{4.0},
-        std::vector<float>{4.0},
-    }};
+    TestHeadlessPrint();
+    TestEmptyPrint();
 }
