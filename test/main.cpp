fec9d21d60d5b10304e3dc13cfb77a069598cc26
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-23 09:42:53 -0400

    tmp: stub small test

--- test/main.cpp
+++ test/main.cpp
@@ -1,1 +1,12 @@
-int main(void) {}
+#include <Dataframe.hpp>
+#include <vector>
+int main(void) {
+  std::vector<std::vector<float>> small{
+      {3, 3},
+      {4, 4},
+  };
+
+  read_file::Dataframe simple{small};
+
+  std::cout << simple << std::endl;
+}
