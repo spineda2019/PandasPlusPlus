3a61863f6b1ca293908005ab4f475ae5f45ed103
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-24 09:27:08 -0400

    refactor to use New factory

--- test/main.cpp
+++ test/main.cpp
@@ -6,34 +6,62 @@
 #include <string_view>
 #include <vector>
 
-void TestHeadlessPrint() {
+bool TestHeadlessPrint() {
     std::vector<std::vector<float>> data{
         {3.0, 4.0},
         {3.0, 4.0},
     };
 
-    ppp::Matrix lvalue{data};
+    std::optional<ppp::Matrix<float>> lvalue{ppp::Matrix<float>::New(data)};
 
-    data = {};
-
-    std::cout << lvalue << std::endl;
+    if (lvalue.has_value()) {
+        std::cout << "Test: TestHeadlessPrint Passed!" << std::endl
+                  << "Matrix Used: " << std::endl
+                  << std::endl;
+        std::cout << lvalue.value() << std::endl;
+        return true;
+    } else {
+        std::cout << "Test: TestHeadlessPrint Failed..." << std::endl
+                  << std::endl;
+        return false;
+    }
 }
 
-void TestEmptyPrint() {
-    ppp::Matrix<float> empty{};
+bool TestEmptyPrint() {
+    std::optional<ppp::Matrix<float>> empty{ppp::Matrix<float>::New()};
 
-    std::cout << empty << std::endl;
+    if (empty.has_value()) {
+        std::cout << "Test: TestEmptyPrint Passed!" << std::endl
+                  << "Matrix Used: " << std::endl
+                  << std::endl;
+        std::cout << empty.value() << std::endl;
+        return true;
+    } else {
+        std::cout << "Test: TestEmptyPrint Failed..." << std::endl << std::endl;
+        return false;
+    }
 }
 
-void TestComplexPrint() {
+bool TestComplexPrint() {
     std::vector<std::vector<std::complex<float>>> complex{{
         {std::complex<float>(3.0, 3.0), std::complex<float>(4.0, 4.0)},
         {std::complex<float>(5.0, 5.0), std::complex<float>(6.0, 6.0)},
     }};
 
-    ppp::Matrix<std::complex<float>> cmp{complex};
+    std::optional<ppp::Matrix<std::complex<float>>> cmp{
+        ppp::Matrix<std::complex<float>>::New(complex)};
 
-    std::cout << cmp << std::endl;
+    if (cmp.has_value()) {
+        std::cout << "Test: TestComplexPrint Passed!" << std::endl
+                  << "Matrix Used: " << std::endl
+                  << std::endl;
+        std::cout << cmp.value() << std::endl;
+        return true;
+    } else {
+        std::cout << "Test: TestComplexPrint Failed..." << std::endl
+                  << std::endl;
+        return false;
+    }
 }
 
 bool TestInsertingHeaders() {
@@ -42,13 +70,25 @@ bool TestInsertingHeaders() {
         {3.0, 4.0},
     };
 
-    ppp::Matrix lvalue{data};
+    std::optional<ppp::Matrix<float>> lvalue{ppp::Matrix<float>::New(data)};
+    if (!lvalue.has_value()) {
+        std::cout << "Test: TestInsertingHeaders Failed..." << std::endl
+                  << std::endl;
+        return false;
+    }
+
     std::vector<std::string_view> headers{"Foo", "Bar"};
-    if (const std::optional<std::uint8_t> result{lvalue.SetHeaders(headers)};
+    if (const std::optional<std::uint8_t> result{
+            lvalue.value().SetHeaders(headers)};
         result.has_value()) {
-        std::cout << lvalue << std::endl;
+        std::cout << "Test: TestInsertingHeaders Passed!" << std::endl
+                  << "Matrix Used: " << std::endl
+                  << std::endl;
+        std::cout << lvalue.value() << std::endl;
         return true;
     } else {
+        std::cout << "Test: TestInsertingHeaders Failed..." << std::endl
+                  << std::endl;
         return false;
     }
 }
