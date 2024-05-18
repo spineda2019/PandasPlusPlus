9444c985b6cf73fd718348f9d4a0cab8cd0fb9c5
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-18 13:52:46 -0400

    stub some col tests

--- test/src/column_tests.cpp
+++ test/src/column_tests.cpp
@@ -1,3 +1,27 @@
-#include <ppp/Column.hpp>
+#include "include/column_tests.hpp"
 
-bool ColumnMasterTest(void) { return true; }
+#include <memory>
+#include <ppp/Column.hpp>
+#include <vector>
+
+namespace column_test {
+
+namespace {
+bool TestConstruction(const std::unique_ptr<std::size_t>& passes,
+                      const std::unique_ptr<std::size_t>& fails) {
+    std::vector<int> data{1, 5, 6};
+
+    ppp::Column col{data, "Key"};
+
+    (*passes)++;
+
+    return true;
+}
+}  // namespace
+
+bool ColumnMasterTest(const std::unique_ptr<std::size_t>& passes,
+                      const std::unique_ptr<std::size_t>& fails) {
+    return TestConstruction(passes, fails);
+}
+
+}  // namespace column_test
--- test/src/include/column_tests.hpp
+++ test/src/include/column_tests.hpp
@@ -1,6 +1,14 @@
 #ifndef TEST_SRC_INCLUDE_COLUMN_TESTS_HPP_
 #define TEST_SRC_INCLUDE_COLUMN_TESTS_HPP_
 
-bool ColumnMasterTest(void);
+#include <cstddef>
+#include <memory>
+
+namespace column_test {
+
+bool ColumnMasterTest(const std::unique_ptr<std::size_t>& passes,
+                      const std::unique_ptr<std::size_t>& fails);
+
+}
 
 #endif  // TEST_SRC_INCLUDE_COLUMN_TESTS_HPP_
