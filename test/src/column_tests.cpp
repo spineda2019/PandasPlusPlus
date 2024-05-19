796e42a6aa8f33c847150ecdebca19604cd3f0e2
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-19 13:54:05 -0400

    Test Construction and printing

--- test/src/column_tests.cpp
+++ test/src/column_tests.cpp
@@ -1,12 +1,27 @@
 #include "include/column_tests.hpp"
 
 #include <memory>
-#include <ppp/Column.hpp>
+#include <string_view>
 #include <vector>
+
+#include "ppp/Column.hpp"
 
 namespace column_test {
 
 namespace {
+
+template <ppp::BasicEntry T>
+inline void PassNotification(const ppp::Column<T>& col,
+                             const std::string_view test_name) {
+    std::cout << test_name << " Passed! Column Used:" << std::endl << col;
+}
+
+template <ppp::BasicEntry T>
+inline void FailNotification(const ppp::Column<T>& col,
+                             const std::string_view test_name) {
+    std::cout << test_name << " Failed... Column Used:" << std::endl << col;
+}
+
 bool TestConstruction(const std::unique_ptr<std::size_t>& passes,
                       const std::unique_ptr<std::size_t>& fails) {
     std::vector<int> data{1, 5, 6};
@@ -14,6 +29,8 @@ bool TestConstruction(const std::unique_ptr<std::size_t>& passes,
     ppp::Column col{data, "Key"};
 
     (*passes)++;
+
+    PassNotification(col, "TestConstruction");
 
     return true;
 }
