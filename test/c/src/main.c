b74304b83b05610fb4c600e9b524b1c570ddf9bc
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-21 10:34:18 -0400

    succesfully test printing and construction from c

--- test/c/src/main.c
+++ test/c/src/main.c
@@ -1,6 +1,12 @@
 #include "CInterface.h"
 
 int main(int argc, char** argv) {
-    FColumn_t foo;
+    float nums[3] = {3.0f, 2.0f, 6.7f};
+    const char key[] = "Column";
+
+    FColumnHandle col = NewFColumn(nums, 3, key);
+
+    PrintFColumn(col);
+
     return 0;
 }
