d27ffd7382689db6d76bd4c813047307101fff9d
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-21 10:11:18 -0400

    include only C System Headers

--- c-exports/src/include/CInterface.h
+++ c-exports/src/include/CInterface.h
@@ -1,9 +1,8 @@
 #ifndef CSVLIBRARY_CINTERFACE_H
 #define CSVLIBRARY_CINTERFACE_H
 
+#include <stddef.h>
 #include <stdint.h>
-
-#include <cstddef>
 
 #if defined _WIN32 || defined __CYGWIN__              // *
 #ifdef __GNUC__                                       // **
