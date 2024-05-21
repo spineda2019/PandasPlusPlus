47bba0be8e7b516ed1466b80bae0b3604a188984
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-21 14:21:56 -0400

    fill out methods for other types

--- c-exports/src/include/ppp_column.h
+++ c-exports/src/include/ppp_column.h
@@ -10,24 +10,57 @@
 extern "C" {
 #endif
 
-#define NewColumnDeclaration(type)                                        \
-    C_API FColumnHandle NewColumn_##type(const type *data, size_t length, \
-                                         const char *key)
-
 typedef void *FColumnHandle;
+typedef void *DColumnHandle;
+typedef void *IColumnHandle;
+typedef void *LColumnHandle;
 
-NewColumnDeclaration(int);
-NewColumnDeclaration(double);
+/* **************************** Column Factories **************************** */
 
 C_API FColumnHandle NewFColumn(const float *data, size_t length,
                                const char *key);
 
+C_API DColumnHandle NewDColumn(const double *data, size_t length,
+                               const char *key);
+
+C_API IColumnHandle NewIColumn(const int *data, size_t length, const char *key);
+
+C_API LColumnHandle NewLColumn(const long *data, size_t length,
+                               const char *key);
+
+/* ****************************** Print Column ****************************** */
+
 C_API void PrintFColumn(FColumnHandle column);
+
+C_API void PrintDColumn(DColumnHandle column);
+
+C_API void PrintIColumn(IColumnHandle column);
+
+C_API void PrintLColumn(LColumnHandle column);
+
+/* ******************************* Add Column ******************************* */
 
 C_API FColumnHandle AddFColumns(const FColumnHandle lhs,
                                 const FColumnHandle rhs);
 
+C_API DColumnHandle AddDColumns(const DColumnHandle lhs,
+                                const DColumnHandle rhs);
+
+C_API IColumnHandle AddIColumns(const IColumnHandle lhs,
+                                const IColumnHandle rhs);
+
+C_API LColumnHandle AddLColumns(const LColumnHandle lhs,
+                                const LColumnHandle rhs);
+
+/* ***************************** Delete Column ****************************** */
+
 C_API void DeleteFColumn(const FColumnHandle column);
+
+C_API void DeleteDColumn(const DColumnHandle column);
+
+C_API void DeleteIColumn(const IColumnHandle column);
+
+C_API void DeleteLColumn(const LColumnHandle column);
 
 #ifdef __cplusplus
 }
--- c-exports/src/ppp_column.cpp
+++ c-exports/src/ppp_column.cpp
@@ -9,49 +9,111 @@
 
 #define EXTRACT_OBJECT(obj, type) *(reinterpret_cast<ppp::Column<type> *>(obj))
 
-#define NewColumnDefinition(type)                                          \
-    NewColumnDeclaration(type) {                                           \
-        ppp::Column<type> *col{                                            \
-            new ppp::Column{std::vector<type>{data, data + length}, key}}; \
-                                                                           \
-        return col;                                                        \
-    }
+/* **************************** Column Factories **************************** */
 
-NewColumnDefinition(int);
-NewColumnDefinition(double);
+#define NewColumnImplementation(type)                                  \
+    ppp::Column<type> *col{                                            \
+        new ppp::Column{std::vector<type>{data, data + length}, key}}; \
+                                                                       \
+    return col;
 
 C_API FColumnHandle NewFColumn(const float *data, size_t length,
                                const char *key) {
-    ppp::Column<float> *col{
-        new ppp::Column{std::vector<float>{data, data + length}, key}};
-
-    return col;
+    NewColumnImplementation(float);
 }
+
+C_API DColumnHandle NewDColumn(const double *data, size_t length,
+                               const char *key) {
+    NewColumnImplementation(double);
+}
+
+C_API IColumnHandle NewIColumn(const int *data, size_t length,
+                               const char *key) {
+    NewColumnImplementation(int);
+}
+
+C_API LColumnHandle NewLColumn(const long *data, size_t length,
+                               const char *key) {
+    NewColumnImplementation(long);
+}
+
+/* ****************************** Print Column ****************************** */
+
+#define PrintColumnImplementation(type)                               \
+    if (column) {                                                     \
+        std::cout << *(reinterpret_cast<ppp::Column<type> *>(column)) \
+                  << std::endl;                                       \
+    } else {                                                          \
+        std::cout << "Bad Column Handle..." << std::endl;             \
+    }
 
 C_API void PrintFColumn(FColumnHandle column) {
-    if (column) {
-        std::cout << *(reinterpret_cast<ppp::Column<float> *>(column))
-                  << std::endl;
-    } else {
-        std::cout << "Bad Column Handle..." << std::endl;
-    }
+    PrintColumnImplementation(float);
 }
+
+C_API void PrintDColumn(DColumnHandle column) {
+    PrintColumnImplementation(double);
+}
+
+C_API void PrintIColumn(IColumnHandle column) {
+    PrintColumnImplementation(int);
+}
+
+C_API void PrintLColumn(LColumnHandle column) {
+    PrintColumnImplementation(long);
+}
+
+/* ******************************* Add Column ******************************* */
+
+#define AddColumnImplementation(type)                                   \
+    if (!lhs || !rhs) {                                                 \
+        return nullptr;                                                 \
+    }                                                                   \
+    auto sum = EXTRACT_OBJECT(lhs, float) + EXTRACT_OBJECT(rhs, float); \
+    if (sum.has_value()) {                                              \
+        return new ppp::Column<float>{std::move(sum.value())};          \
+    } else {                                                            \
+        return nullptr;                                                 \
+    }
 
 C_API FColumnHandle AddFColumns(const FColumnHandle lhs,
                                 const FColumnHandle rhs) {
-    if (!lhs || !rhs) {
-        return nullptr;
-    }
-    auto sum = EXTRACT_OBJECT(lhs, float) + EXTRACT_OBJECT(rhs, float);
-    if (sum.has_value()) {
-        return new ppp::Column<float>{std::move(sum.value())};
-    } else {
-        return nullptr;
-    }
+    AddColumnImplementation(float);
 }
 
-C_API void DeleteFColumn(const FColumnHandle column) {
-    if (column) {
-        delete reinterpret_cast<ppp::Column<float> *>(column);
+C_API DColumnHandle AddDColumns(const DColumnHandle lhs,
+                                const DColumnHandle rhs) {
+    AddColumnImplementation(double);
+}
+
+C_API IColumnHandle AddIColumns(const IColumnHandle lhs,
+                                const IColumnHandle rhs) {
+    AddColumnImplementation(int);
+}
+
+C_API LColumnHandle AddLColumns(const LColumnHandle lhs,
+                                const LColumnHandle rhs) {
+    AddColumnImplementation(long);
+}
+
+/* ***************************** Delete Column ****************************** */
+
+#define DeleteColumnImplementation(type)                      \
+    if (column) {                                             \
+        delete reinterpret_cast<ppp::Column<type> *>(column); \
     }
+
+C_API void DeleteFColumn(const FColumnHandle column) {
+    DeleteColumnImplementation(float);
+}
+
+C_API void DeleteDColumn(const DColumnHandle column) {
+    DeleteColumnImplementation(double);
+}
+C_API void DeleteIColumn(const IColumnHandle column) {
+    DeleteColumnImplementation(int);
+}
+
+C_API void DeleteLColumn(const LColumnHandle column) {
+    DeleteColumnImplementation(long);
 }
