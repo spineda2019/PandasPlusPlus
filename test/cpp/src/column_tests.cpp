7b360a1d54204e6604b42b2b50d4778d8277fbec
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-25 07:53:05 -0400

    test dot product operator

--- test/cpp/src/column_tests.cpp
+++ test/cpp/src/column_tests.cpp
@@ -36,6 +36,39 @@ bool TestConstruction(const std::unique_ptr<std::size_t>& passes,
     return true;
 }
 
+bool TestDot(const std::unique_ptr<std::size_t>& passes,
+             const std::unique_ptr<std::size_t>& fails) {
+    std::vector<int> data{1, 5, 6};
+    std::vector<int> short_data{1, 5};
+
+    ppp::Column col{data, "Key"};
+    ppp::Column short_col{short_data, "Key"};
+
+    std::optional<int> dot = col * short_col;
+
+    if (dot.has_value()) {
+        FailNotification(col, "TestDot");
+        (*fails)++;
+        return false;
+    }
+
+    std::optional<int> good_dot = col * col;
+
+    if (!good_dot.has_value()) {
+        FailNotification(col, "TestDot");
+        (*fails)++;
+        return false;
+    } else if (good_dot.value() != 62) {
+        FailNotification(col, "TestDot");
+        (*fails)++;
+        return false;
+    } else {
+        PassNotification(col, "TestDot");
+        (*passes)++;
+        return true;
+    }
+}
+
 bool TestAddition(const std::unique_ptr<std::size_t>& passes,
                   const std::unique_ptr<std::size_t>& fails) {
     std::vector<int> data{1, 5, 6};
@@ -183,7 +216,8 @@ bool ColumnMasterTest(const std::unique_ptr<std::size_t>& passes,
                       const std::unique_ptr<std::size_t>& fails) {
     return TestConstruction(passes, fails) && TestAddition(passes, fails) &&
            TestIndexing(passes, fails) && TestSum(passes, fails) &&
-           TestComparison(passes, fails) && TestSubtraction(passes, fails);
+           TestComparison(passes, fails) && TestSubtraction(passes, fails) &&
+           TestDot(passes, fails);
 }
 
 }  // namespace column_test
